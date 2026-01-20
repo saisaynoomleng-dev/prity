868bdd0a209a13d2b51fca74045c57c3dcd4cfd6
saisaynoomleng-dev <snoomleng1@cougars.ccis.edu>
2026-01-20 16:48:05 -0500

    finish auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,17 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPulicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api(.*)",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!isPulicRoute(req)) {
+    auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
